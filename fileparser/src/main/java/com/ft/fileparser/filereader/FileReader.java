33a43a91c0ae92ea22c529fad6bace0034730506
francistalampas <70066555+francistalampas@users.noreply.github.com>
2023-03-01 03:08:41 +0800

    Delete fileparser directory

--- fileparser/src/main/java/com/ft/fileparser/filereader/FileReader.java
+++
@@ -1,8 +0,0 @@
-package com.ft.fileparser.filereader;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-public interface FileReader {
-    String readFile(Path filePath) throws IOException;
-}
